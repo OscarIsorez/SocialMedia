863525f6b8c81c76c3a922bd664b6573391b378f
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2023-10-27 19:12:13 +0200

    update

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from kivymd.app import MDApp
 from kivymd.uix.textfield import MDTextField
 from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.bottomnavigation import MDBottomNavigation, MDBottomNavigationItem
+from kivymd.uix.label import MDLabel
+from kivymd.uix.imagelist.imagelist import MDSmartTile
+from kivymd.uix.button import MDIconButton
+from kivymd.uix.boxlayout import MDBoxLayout
 
 from kivy.properties import ObjectProperty, StringProperty
 
@@ -75,6 +79,13 @@ BoxLayout:
             text: "Page 1"
             icon: 'greenhouse'
 
+            ScrollView:
+                GridLayout:
+                    id:grid
+                    cols: 1
+                    size_hint_y: None
+                    height: self.minimum_height
+
         MDBottomNavigationItem:
             name: 'screen 2'
             text: "Page 2"
@@ -86,6 +97,7 @@ BoxLayout:
             name: 'screen 3'
             text: "Page 3"
             icon: 'plus-box'
+            on_tab_release: root.on_enter_tab3()
     
         MDBottomNavigationItem:
             name: 'screen 4'
@@ -157,6 +169,83 @@ class MainScreen(Screen):
     profilename = ObjectProperty(None)
     email = ObjectProperty(None)
     password = ObjectProperty(None)
+
+    def on_enter(self):
+        grid = self.ids.grid
+
+        grid.clear_widgets()
+        grid.height = self.height
+        layout = MDBoxLayout(orientation="vertical", spacing=10)
+
+        for i in range(10):
+            smart_tile = MDSmartTile(
+                radius=24,
+                box_radius=[0, 0, 24, 24],
+                box_color=(1, 1, 1, 0.2),
+                source="cat.jpg",
+                pos_hint={"center_x": 0.5, "center_y": 0.5},
+                size_hint=(1, None),
+                height="320dp",
+                size=("320dp", "320dp"),
+            )
+
+            icon_button = MDIconButton(
+                icon="heart-outline",
+                theme_text_color="Custom",
+                text_color=(1, 0, 0, 1),
+                pos_hint={"center_y": 0.5},
+            )
+            icon_button.bind(
+                on_release=lambda instance: setattr(
+                    icon_button,
+                    "icon",
+                    "heart" if icon_button.icon == "heart-outline" else "heart-outline",
+                )
+            )
+
+            label = MDLabel(text="Julia and Julie", bold=True, color=(1, 1, 1, 1))
+
+            smart_tile.add_widget(icon_button)
+            smart_tile.add_widget(label)
+
+            layout.add_widget(smart_tile)
+
+        grid.add_widget(layout)
+
+    def add_smart_tile(self):
+        smart_tile = MDSmartTile(
+            radius=24,
+            box_radius=[0, 0, 24, 24],
+            box_color=(1, 1, 1, 0.2),
+            source="cat.jpg",
+            pos_hint={"center_x": 0.5, "center_y": 0.5},
+            size=("320dp", "320dp"),
+        )
+
+        icon_button = MDIconButton(
+            icon="heart-outline",
+            theme_text_color="Custom",
+            text_color=(1, 0, 0, 1),
+            pos_hint={"center_y": 0.5},
+        )
+        icon_button.bind(
+            on_release=lambda instance: setattr(
+                icon_button,
+                "icon",
+                "heart" if icon_button.icon == "heart-outline" else "heart-outline",
+            )
+        )
+
+        label = MDLabel(text="Julia and Julie", bold=True, color=(1, 1, 1, 1))
+
+        smart_tile.add_widget(icon_button)
+        smart_tile.add_widget(label)
+
+        self.ids.grid.add_widget(smart_tile)
+        ic("Smart tile added !")
+
+    def on_enter_tab3(self):
+        self.add_smart_tile()
 
     def on_enter_tab4(self):
         self.ids.displayInfos.text = "Bienvenue, " + str(
